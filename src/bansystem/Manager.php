ea543cb3f3fc3fdeb567e24bc8cc27f70f3e49a1
KingBob <52259912+Kingbobs@users.noreply.github.com>
2023-06-23 19:32:34 -0600

    Update Manager.php

--- src/bansystem/Manager.php
+++ src/bansystem/Manager.php
@@ -7,25 +7,25 @@ use bansystem\permission\MuteList;
 
 class Manager {
     
-    public static function getNameMutes() : MuteList {
+    public static function getNameMutes(): MuteList {
         $muteList = new MuteList("muted-players.txt");
         $muteList->load();
         return $muteList;
     }
     
-    public static function getIPMutes() : MuteList {
+    public static function getIPMutes(): MuteList {
         $muteList = new MuteList("muted-ips.txt");
         $muteList->load();
         return $muteList;
     }
     
-    public static function getNameBlocks() : BlockList {
+    public static function getNameBlocks(): BlockList {
         $blockList = new BlockList("blocked-players.txt");
         $blockList->load();
         return $blockList;
     }
     
-    public static function getIPBlocks() : BlockList {
+    public static function getIPBlocks(): BlockList {
         $blockList = new BlockList("blocked-ips.txt");
         $blockList->load();
         return $blockList;
